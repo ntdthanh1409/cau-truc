edb8e80d03e90520c29f1d62edaa10d333cfa7e8
ntdthanh1409 <72120867+ntdthanh1409@users.noreply.github.com>
2021-05-04 09:26:29 +0700

    Update sắp-xếp-nổi-bọt.r

--- sắp-xếp-nổi-bọt.r
+++ sắp-xếp-nổi-bọt.r
@@ -1,5 +1,4 @@
-#sap xep noi bot
-bubble <- function(x){
+bubble_sort <- function(x){
   n<-length(x)
   for(j in 1:(n-1)){
     for(i in 1:(n-j)){
@@ -12,4 +11,5 @@ bubble <- function(x){
   }
   return(x)
 }
-bubble(dulieu)
+x <- c(32, 17, 49, 98, 06, 25, 53, 61)
+bubble_sort(x)
